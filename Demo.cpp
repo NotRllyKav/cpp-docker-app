b666329ae5dc6c523243e395b1bed2b9423ca4da
Kav Karun <notrllykav@Kavs-Laptop.local>
2025-04-23 22:51:35 -0400

    another test

--- Demo.cpp
+++ Demo.cpp
@@ -18,7 +18,7 @@ public:
     }
 
     ~Player() {
-        delete score; // free memory
+        delete score; // free memory another test
         cout << "[LOG] Player object destroyed: " << name << endl;
     }
 
